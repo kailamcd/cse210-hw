6022ffdf1482083bfc67ff1311b97ecd192b75dd
kailamcd <mcd.kaila@gmail.com>
2023-01-07 11:42:57 -0600

    prep 1 complete

---
+++ w01_practice.cs
@@ -0,0 +1,14 @@
+int x = 17;
+Console.Writeline(x);
+Console.Writeline("Well, that's it!");
+Console.Write("No newline, what's your favorite color? ");
+user_fave_color = Console.Readline();
+Console.Writeline(user_fave_color);
+if (user_fave_color === "pink")
+{
+    Console.Writeline("Mine, too!");
+}
+else
+{
+    Console.Write($"{user_fave_color} isn't my favorite, but I do like it!");
+}
